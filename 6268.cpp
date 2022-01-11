69e67022beb5787c7853431faa7bed139ca00c11
Ali Mohamed <67805101+Alimohamad21@users.noreply.github.com>
2022-01-12 01:55:26 +0200

    fifo

--- 6268.cpp
+++ 6268.cpp
@@ -1,59 +1,113 @@
 #include <iostream>
+
 using namespace std;
+
 #include <string>
 # include <vector>
+#include <deque>
+
 void readInputs();
+
 void run();
+
 void fifo();
+
 void lru();
+
 void optimal();
+
 void clockAlgorithm();
-int pageSize;
+
+bool inFrame(vector<string> frameContents, string page);
+
+int maxPageSize;
 string replacementMethod;
-vector<int> pages;
-int main(){
+vector<string> pages;
 
-run();
+int main() {
+    run();
+}
 
-return 0;
+void run() {
+    readInputs();
+    printf("Replacement Policy = %s\n-------------------------------------\nPage   Content of Frames\n----   ---------------\n",
+           replacementMethod.c_str());
+    if (replacementMethod == "FIFO")
+        fifo();
+    else if (replacementMethod == "LRU")
+        lru();
+    else if (replacementMethod == "CLOCK")
+        clockAlgorithm();
+    else if (replacementMethod == "OPTIMAL")
+        optimal();
 }
-void run(){
-readInputs();
 
-for (int i:pages)
-      printf("%d\n",i);
-printf("Replacement Policy = %s\n-------------------------------------\nPage   Content of Frames\n----   ---------------\n",replacementMethod.c_str());
-if (replacementMethod=="FIFO")
-	fifo();
-else if(replacementMethod=="LRU")
-       lru();
-else if(replacementMethod=="CLOCK")
-       clockAlgorithm();
-else if(replacementMethod=="OPTIMAL")
-       optimal();
+void readInputs() {
+    scanf("%d", &maxPageSize);
+    cin >> replacementMethod;
+    string page;
+    cin >> page;
+    while (page != "-1") {
+        if (page.length() == 1)
+            page = "0" + page;
+        pages.push_back(page);
+        cin >> page;
+    }
 }
-void readInputs(){
-scanf("%d",&pageSize);
-cin >> replacementMethod;
-int page;
-vector<int> pages;
-scanf("%d",&page);
-while(page!=-1)
-{  
-   pages.push_back(page);
-   scanf("%d",&page);
+
+bool inFrame(vector<string> frameContents, string page) {
+    for (int i = 0; i < frameContents.size(); i++) {
+        if (frameContents[i] == page)
+            return true;
+    }
+    return false;
 }
+
+void fifo() {
+    vector<string> frameContents;
+    int pageFaults = 0;
+    string lineToPrint;
+    deque<string>::iterator search;
+    bool pageInFrame;
+    int firstIn=0;
+    for (string page:pages) {
+        lineToPrint = page;
+        pageInFrame = inFrame(frameContents, page);
+        if (frameContents.size() < maxPageSize) {
+            if (!pageInFrame)
+                frameContents.push_back(page);// enqueue
+            lineToPrint += "     ";
+        } else if (frameContents.size() == maxPageSize) {
+            if (!pageInFrame) {
+                frameContents[firstIn]=page;
+                firstIn= (firstIn+1) % maxPageSize;
+                pageFaults++;
+                lineToPrint += " F";
+                lineToPrint += "   ";
+            } else {
+                lineToPrint += "     ";
+            }
+        }
+        for (int i = 0; i < frameContents.size(); i++) {
+            lineToPrint += frameContents[i];
+            if (i != frameContents.size() - 1)
+                lineToPrint += " ";
+        }
+        lineToPrint += "\n";
+        cout << lineToPrint;
+    }
+    printf("-------------------------------------\n");
+    printf("Number of page faults = %d\n", pageFaults);
 }
-void fifo()
-{ 
- printf("fif");
+
+void lru() {
+    printf("a");
 }
-void lru(){
- printf("a");
+
+void clockAlgorithm() {
+    printf("a");
 }
-void clockAlgorithm(){
-printf("a");
-}
-void optimal(){
-printf("a");
+
+void optimal() {
+    printf("a");
 }

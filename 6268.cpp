4dd8cb0f41cfc31a9e9c70b2feb99c42164ed36e
Ali Mohamed <67805101+Alimohamad21@users.noreply.github.com>
2022-01-11 17:21:28 +0200

    initializing

---
+++ 6268.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include <unordered_map>
+#include <string>
+void fifo();
+void lru();
+void clock();
+void optimal();
+int main() {
+    return 0;
+}
+void fifo(){
+
+}
+void lru(){
+
+}
+void clock(){
+
+}
+void optimal(){
+
+}

8faf13ce3f78b64201af8242124dcaa10fb2ef74
Ali Mohamed <67805101+Alimohamad21@users.noreply.github.com>
2022-01-11 20:58:18 +0200

    init 2

--- 6268.cpp
+++ 6268.cpp
@@ -1,23 +1,59 @@
 #include <iostream>
 using namespace std;
-#include <unordered_map>
 #include <string>
+# include <vector>
+void readInputs();
+void run();
 void fifo();
 void lru();
-void clock();
 void optimal();
-int main() {
-    return 0;
-}
-void fifo(){
+void clockAlgorithm();
+int pageSize;
+string replacementMethod;
+vector<int> pages;
+int main(){
 
+run();
+
+return 0;
+}
+void run(){
+readInputs();
+
+for (int i:pages)
+      printf("%d\n",i);
+printf("Replacement Policy = %s\n-------------------------------------\nPage   Content of Frames\n----   ---------------\n",replacementMethod.c_str());
+if (replacementMethod=="FIFO")
+	fifo();
+else if(replacementMethod=="LRU")
+       lru();
+else if(replacementMethod=="CLOCK")
+       clockAlgorithm();
+else if(replacementMethod=="OPTIMAL")
+       optimal();
+}
+void readInputs(){
+scanf("%d",&pageSize);
+cin >> replacementMethod;
+int page;
+vector<int> pages;
+scanf("%d",&page);
+while(page!=-1)
+{  
+   pages.push_back(page);
+   scanf("%d",&page);
+}
+}
+void fifo()
+{ 
+ printf("fif");
 }
 void lru(){
-
+ printf("a");
 }
-void clock(){
-
+void clockAlgorithm(){
+printf("a");
 }
 void optimal(){
-
+printf("a");
 }
